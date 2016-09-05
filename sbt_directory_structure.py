ea2e097f25ae73de5008fa72d938ac0c34b9cf97
Miguel <migueln1987@gmail.com>
2016-09-05 18:51:42 -0500

    edited .gitignore to ignore everything but project files and edited sbt_directory_structure.py

--- sbt_directory_structure.py
+++ sbt_directory_structure.py
@@ -1,10 +1,13 @@
 import os
 import sys
 
-root_path = 'src/'
+project_name = 'MyProject'
+root_path = os.path.join(project_name, 'src/')
 folders = ['main', 'test']
 subfolders = ['resources', 'scala', 'java']
 root_folders = ['lib', 'project']
+
+
 if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
     for folder in folders:
         for subfolder in subfolders:
@@ -16,7 +19,17 @@ if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
                 else:
                     print(e)
     for i in root_folders:
-        os.mkdir(os.path.join(root_path, i))
+        try:
+            os.mkdir(os.path.join(project_name, i))
+        except:
+            if FileExistsError:
+                pass
+            else:
+                print(e)
+
+    file = open(os.path.join(project_name,'build.sbt'), 'w')
+    file.write('name:= "MyProject"\nversion := "1.0"\nscalaVersion:= "2.11.8"')
+    file.close()
                  
 if sys.platform.startswith('win32'):
     pass

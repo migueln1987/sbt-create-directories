0b724344113a959fd48dd32a16c273b8166eae18
Miguel <migueln1987@gmail.com>
2016-09-05 18:27:38 -0500

    edited sbt_directory_structure.py

--- sbt_directory_structure.py
+++ sbt_directory_structure.py
@@ -4,6 +4,7 @@ import sys
 root_path = 'src/'
 folders = ['main', 'test']
 subfolders = ['resources', 'scala', 'java']
+root_folders = ['lib', 'project']
 if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
     for folder in folders:
         for subfolder in subfolders:
@@ -14,5 +15,8 @@ if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
                     pass
                 else:
                     print(e)
+    for i in root_folders:
+        os.mkdir(os.path.join(root_path, i))
+                 
 if sys.platform.startswith('win32'):
     pass

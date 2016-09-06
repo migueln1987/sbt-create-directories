48b466677489d4ab2a6e5bca20154afd06138245
Miguel <migueln1987@gmail.com>
2016-09-05 19:32:53 -0500

    deleted if statements in sbt_directory_structure.py

--- sbt_directory_structure.py
+++ sbt_directory_structure.py
@@ -13,28 +13,24 @@ subfolders = ['resources', 'scala', 'java']
 root_folders = ['lib', 'project']
 
 
-if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-    for folder in folders:
-        for subfolder in subfolders:
-            try:
-                os.makedirs(os.path.join(root_path, folder, subfolder))
-            except:
-                if FileExistsError:
-                    pass
-                else:
-                    print(e)
-    for i in root_folders:
+for folder in folders:
+    for subfolder in subfolders:
         try:
-            os.mkdir(os.path.join(project_name, i))
+            os.makedirs(os.path.join(root_path, folder, subfolder))
         except:
             if FileExistsError:
                 pass
             else:
                 print(e)
+for i in root_folders:
+    try:
+        os.mkdir(os.path.join(project_name, i))
+    except:
+        if FileExistsError:
+            pass
+        else:
+            print(e)
 
-    file = open(os.path.join(project_name,'build.sbt'), 'w')
-    file.write('name:= "{}"\nversion := "1.0"\nscalaVersion:= "2.11.8"'.format(project_name))
-    file.close()
-                 
-if sys.platform.startswith('win32'):
-    pass
+file = open(os.path.join(project_name,'build.sbt'), 'w')
+file.write('name:= "{}"\nversion := "1.0"\nscalaVersion:= "2.11.8"'.format(project_name))
+file.close()
